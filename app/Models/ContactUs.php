1956e714ff37821051ab8c7e2d61c3b4aae2f5ec
Gerry0303 <92528396+gurusingagerry03@users.noreply.github.com>
2024-03-15 17:04:47 +0700

    Add files via upload

---
+++ app/Models/ContactUs.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactUs extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+    protected $table = 'contactus';
+    protected $fillable = [
+        'input_date',
+        'sender_name',
+        'Sender_institution',
+        'contact_number',
+        'type',
+        'Subyek',
+        'Content',
+        'id_Grant_collaborator_category',
+        'status'
+    ];
+}
