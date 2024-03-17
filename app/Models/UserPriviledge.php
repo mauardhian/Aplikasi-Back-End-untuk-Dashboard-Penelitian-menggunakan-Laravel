5b5ac86544a3650eef8e3b563ebe71b0964cfc12
Alif T <alift929@gmail.com>
2024-03-17 07:09:45 +0700

    feat: User Priviledge Service

---
+++ app/Models/UserPriviledge.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPriviledge extends Model
+{
+    use HasFactory;
+
+    // Tabel User_Priviledge
+    // protected $connection =
+    protected $table = 'User_Priviledge';
+    public $timestamps = false;
+    // const CREATED_AT = 'created_at';
+    // const UPDATED_AT = 'updated_at';
+    // protected $primaryKey = 'id_author';
+    // public $incrementing = false; 
+
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+}
