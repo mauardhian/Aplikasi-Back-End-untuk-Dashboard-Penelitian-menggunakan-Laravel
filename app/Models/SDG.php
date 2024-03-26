9c8ef2f1ac20f874f1c02488fbcf292fbcb0a323
Gerry0303 <92528396+gurusingagerry03@users.noreply.github.com>
2024-03-26 19:57:09 +0700

    Add files via upload

---
+++ app/Models/SDG.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SDG extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'id';
+    protected $table = 'sdgs';
+    protected $fillable = [
+        'name'
+    ];
+
+}
