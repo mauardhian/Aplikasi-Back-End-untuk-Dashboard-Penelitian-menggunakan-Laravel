21658995c163a15bd292bc615d1a34bd2e35d49d
billyyuriaan <billy.yuriaan72@gmail.com>
2024-06-21 12:36:58 +0700

    [add]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        Passport::ignoreRoutes();
+        //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        Passport::ignoreMigrations();
+        //
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,30 +9,11 @@ use App\Http\Controllers\ReadController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// Route::post('GetScopusDoc', [ReadController::class, 'getScopusDoc'])->middleware(['auth:api','scope:old-pegawai']);
-
-Route::post('GetScopusDoc', [ReadController::class, 'getScopusDoc']);
-Route::post('getAuthorScopusDoc', [ReadController::class, 'getAuthorScopusDoc']);
-Route::post('LoginSinta', [ReadController::class, 'loginSinta']);
-//Author
-Route::post('getDaftarAuthor', [ReadController::class, 'getDaftarAuthor']);
-Route::post('getDaftarAfiliasi', [ReadController::class, 'getDaftarAfiliasi']);
-Route::post('getAuthorWosDoc', [ReadController::class, 'getAuthorWosDoc']);
-Route::post('getAuthorGaruda', [ReadController::class, 'getAuthorGaruda']);
-Route::post('getAuthorGoogle', [ReadController::class, 'getAuthorGoogle']);
-Route::post('getAuthorResearch', [ReadController::class, 'getAuthorResearch']);
-Route::post('getAuthorIpr', [ReadController::class, 'getAuthorIpr']);
-Route::post('getAuthorBookDoc', [ReadController::class, 'getAuthorBookDoc']);
-Route::post('getAuthorComServiceDoc', [ReadController::class, 'getAuthorComServiceDoc']);
-Route::post('getDaftarJurnal', [ReadController::class, 'getDaftarJurnal']);
-//Afiliasi
-// Route::post('getAfiliasiScopusDoc', [ReadController::class, 'getAfiliasiScopusDoc']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
